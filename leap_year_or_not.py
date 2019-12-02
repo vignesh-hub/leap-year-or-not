20db472ec0324a9890cd78b32643881d99ea761d
vignesh-hub <58372359+vignesh-hub@users.noreply.github.com>
2019-12-02 12:49:30 +0530

    leap year or not
    simplee program to the year is leap year or not

---
+++ leap_year_or_not.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 23:11:13 2019
+
+@author: DELL
+"""
+print("to check the year is leap or not")
+n=input("enter the year you need to check out?\n")
+c=int(n)
+if(c%4==0):
+    print("the year",c,"is leap year")
+else:
+    print("the year",c,"is not an leap year")
